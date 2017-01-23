0898a1b8e9db4f171d059daff791dc1e4cf4ffb1
ucunurul <ucu.nurul.ulum@basajans.com>
2017-01-23 10:57:05 +0700

    New logic in server.js

--- Server.js
+++ Server.js
@@ -1,6 +1,7 @@
 var exp = require("express");
 var app = exp();
 var router = exp.Router();
+var assert = require ('assert');
 
 //Store all HTML files in view folder.
 app.use(exp.static(__dirname + '/View'));
@@ -16,11 +17,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+        });
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({
+    extended:true
+}));
 
 var db;
 // init mongodb
 var Mongoc = require("mongodb").MongoClient;
-var mongourl = 'mongodb://192.168.100.36:27017/SchoolDB';
+// var mongourl = 'mongodb://192.168.100.36:27017/SchoolDB';
+var mongourl = 'mongodb://localhost:27017/SchoolDB';
 Mongoc.connect(mongourl, function(err, database){
     if(err){
         console.log("Mongo is not connected!");
@@ -37,16 +48,21 @@ Mongoc.connect(mongourl, function(err, database){
     }
 })
 
-// index navigation
-app.get('/', function(req, res){
-
-      res.sendFile(path.join(__dirname + '/index.html'));
-
-    // res.json({
-    //     name: 'ucu',
-    //     age: 21
-    // })
+router.get('/', function(req, res, next){
+    res.sendFile(path.join(__dirname + '/index.html'));
+    res.render('Server');
 });
+
+// index navigation
+// app.get('/', function(req, res){
+
+//       res.sendFile(path.join(__dirname + '/index.html'));
+
+//     // res.json({
+//     //     name: 'ucu',
+//     //     age: 21
+//     // })
+// });
 
 // student navigation
 app.get('/student', function(req, res){
@@ -95,22 +111,55 @@ app.get('/hello', function(req, res){
 
 // Get student data
 app.get('/getStudent', function(req, res){
-         db.collection('student', function(err, collection) {
-             collection.find().toArray(function(err, items) {
-                 console.log(items);
-                 res.send(items);
-             });
-         });
+    var student = db.collection('student');
+    student.find().toArray(function(err, docs){
+        console.log('fetching data');
+        res.json(docs);
+
+    });
+
+    // before changed
+
+        //  db.collection('student', function(err, collection) {
+        //      collection.find().toArray(function(err, items) {
+        //          console.log(items);
+        //          res.send(items);
+        //      });
+        //  });
      });
 
 // Post student data
-app.post('/addStudent', (req, res) => {
-  db.collection('student').save(req.body, (err, result) => {
-    if (err) return console.log(err)
-    console.log(req.body)
-    // console.log('Saved to database!')
-    res.redirect('/')
-  })
+// Version 1
+// app.post('/addStudent', (req, res) => {
+//   db.collection('student').save(req.body, (err, result) => {
+//     if (err) return console.log(err)
+//     console.log(req.body)
+//     console.log('Saved to database!')
+//     res.redirect('/')
+//   })
+// })
+
+// Version 2
+app.post('/addStudent', function(req, res, next){
+    console.log('Processing');
+
+    // var hasil = json.body
+
+    var newStudent = {
+        studentid:req.body.studentid,
+        name:req.body.name,
+        class:req.body.class,
+        age:req.body.age,
+        gender:req.body.gender,
+        address:req.body.address
+
+    };
+    
+    db.collection('student').insert(newStudent, function(err, result){
+        assert.equal(null, err);
+        console.log('New student inserted');        
+    });
+    res.json(newStudent);
 })
 
 app.get("/user/:name", function(req,res){
